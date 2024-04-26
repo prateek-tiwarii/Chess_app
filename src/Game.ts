3466586f6fa2ce66867491843838e4ae47da0046
prateek tiwari <tiwariprateek1976@gmail.com>
2024-04-26 16:07:48 +0530

    game logic

--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,6 @@
 import { WebSocket } from "ws";
 import { Chess } from 'chess.js'
+import { GAME_OVER, MOVE } from "./messages";
 
 
 
@@ -8,17 +9,66 @@ export class Game{
     public player1: WebSocket;
     public player2: WebSocket;
     public board : Chess;
-    private moves : string[];
+    private startTime : Date;
+
 
 
 
     constructor(player1 : WebSocket, player2: WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
         this.startTime = new Date();
         
+
+    }
+
+    makeMove(socket : WebSocket , move:{
+        from :string,
+        to: string
+    }){
+
+
+        if(this.board.move.length %2 == 0 && socket != this.player1){
+            return;
+        }
+
+        if(this.board.move.length %2 == 0 && socket != this.player1){
+            return;
+        }
+
+        try {
+            this.board.move(move)
+        } catch (error) {
+            return;
+        }
+
+        if(this.board.isGameOver()){
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload : {
+                    winner : this.board.turn() === "w"? "black" : "white"
+                }
+            }));
+            this.player2.emit("Game Over");
+            return;
+        }
+
+        if(this.board.move.length%2 == 0){
+            this.player2.emit(JSON.stringify({
+                type : MOVE,
+                payload: move
+            }))
+        }
+
+        else{
+
+            this.player1.emit(JSON.stringify({
+                type : MOVE,
+                payload: move
+            }))
+
+        }
 
     }
 }
